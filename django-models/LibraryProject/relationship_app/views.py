e854cb3534cbcb177c1b5d7c0ec5fe044078e498
OojayFidel <odeyjonathan008@gmail.com>
2026-01-19 10:11:46 +0100

    bug fixes

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,14 +2,12 @@ from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Book, Library
 
-# Function-based view: list all books
 def list_books(request):
-    books = Book.objects.select_related("author").all()
-    return render(request, "list_books.html", {"books": books})
+    books = Book.objects.all()
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
 
-# Class-based view: show details for ONE library + its books
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "library_detail.html"
+    template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
