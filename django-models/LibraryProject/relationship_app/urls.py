3282415620c91c157ab87c652cf8f940c8f2fc1e
OojayFidel <odeyjonathan008@gmail.com>
2026-01-19 10:58:47 +0100

    Fourth task

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
     path("logout/", auth_views.LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
     path("register/", register, name="register"),
     path("register/", views.register, name="register"),
+    path("admin/", views.admin_view, name="admin_view"),
+    path("librarian/", views.librarian_view, name="librarian_view"),
+    path("member/", views.member_view, name="member_view"),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
+from django.contrib.auth.decorators import user_passes_test
 
 from .models import Book
 from .models import Library
@@ -29,4 +30,28 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, "relationship_app/register.html", {"form": form})
+
+def is_admin(user):
+    return user.userprofile.role == "Admin"
+
+def is_librarian(user):
+    return user.userprofile.role == "Librarian"
+
+def is_member(user):
+    return user.userprofile.role == "Member"
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "admin_view.html")
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "librarian_view.html")
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "member_view.html")
 
