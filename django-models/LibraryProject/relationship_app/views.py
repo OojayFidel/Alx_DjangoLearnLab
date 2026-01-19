26cca7ae6517d4c67663858723051c1e213dc1fd
OojayFidel <odeyjonathan008@gmail.com>
2026-01-19 11:12:35 +0100

    Fifth task

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
 from .models import Book
 from .models import Library
@@ -54,4 +56,16 @@ def librarian_view(request):
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, "relationship_app/member_view.html")
+
+@permission_required("relationship_app.can_add_book", raise_exception=True)
+def add_book(request):
+    return render(request, "relationship_app/add_book.html")
+
+@permission_required("relationship_app.can_change_book", raise_exception=True)
+def edit_book(request, pk):
+    return render(request, "relationship_app/edit_book.html")
+
+@permission_required("relationship_app.can_delete_book", raise_exception=True)
+def delete_book(request, pk):
+    return render(request, "relationship_app/delete_book.html")
 
