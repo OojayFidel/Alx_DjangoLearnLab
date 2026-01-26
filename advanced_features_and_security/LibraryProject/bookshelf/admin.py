73aab53080e62ee169a6aa445bc7e1e371a6f48e
OojayFidel <odeyjonathan008@gmail.com>
2026-01-26 09:05:34 +0100

    django week 3 first task

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -7,3 +7,20 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     list_filter = ('author', 'publication_year')
     search_fields = ('title', 'author')
+
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Fields", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+
+    list_display = ("username", "email", "is_staff", "date_of_birth")
+
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -8,3 +8,17 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title  # Return the book title as its string representation
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to="profile_photos/", null=True, blank=True)
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
