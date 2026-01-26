e52d49c96ce6afc140732a093c1f34eccaa8b955
OojayFidel <odeyjonathan008@gmail.com>
2026-01-26 09:07:39 +0100

    bug fix

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -11,7 +11,30 @@ class Book(models.Model):
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from .managers import CustomUserManager
+
+
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        if not username:
+            raise ValueError("The Username must be set")
+
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        return self.create_user(username, email, password, **extra_fields)
 
 
 class CustomUser(AbstractUser):
