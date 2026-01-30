c59c9fd9401c5839869ce9ef24b5835bd146eac2
OojayFidel <odeyjonathan008@gmail.com>
2026-01-30 10:16:47 +0100

    Built first api endpoint with DRF

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Set up a view that uses the serializer to retrieve and return book data.
+
+from rest_framework.generics import ListAPIView
+from .models import Book
+from .serializers import BookSerializer
+
+class BookList(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
