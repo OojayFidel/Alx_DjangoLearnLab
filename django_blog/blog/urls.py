d449365a935b449a29a13dedd7a603c74be96203
OojayFidel <odeyjonathan008@gmail.com>
2026-02-13 10:04:28 +0100

    Replace add_comment with CommentCreateView in blog/urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,7 +7,7 @@ from .views import (
     PostListView, PostDetailView, PostCreateView,
     PostUpdateView, PostDeleteView
 )
-from .views import add_comment, CommentUpdateView, CommentDeleteView
+from .views import CommentCreateView, CommentUpdateView, CommentDeleteView
 
 
 urlpatterns = [
@@ -20,7 +20,7 @@ urlpatterns = [
     path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
     path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
     path("posts/", PostListView.as_view(), name="post-list"),
-    path("post/<int:pk>/comment/new/", add_comment, name="add-comment"),
+    path("post/<int:pk>/comment/new/", CommentCreateView.as_view(), name="comment-create"),
     path("comment/<int:pk>/update/", CommentUpdateView.as_view(), name="comment-update"),
     path("comment/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment-delete"),
 
