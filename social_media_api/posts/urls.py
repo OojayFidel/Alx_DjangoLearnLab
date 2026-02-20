67bc07982687faa0999e0b2e87aaa780fa775a86
OojayFidel <odeyjonathan008@gmail.com>
2026-02-20 23:06:07 +0100

    chore: add urls for like/unlike and notifications endpoints

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -10,6 +10,7 @@ urlpatterns = router.urls
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet, FeedView
+from .views import LikePostView, UnlikePostView
 
 router = DefaultRouter()
 router.register(r"posts", PostViewSet, basename="posts")
@@ -17,6 +18,12 @@ router.register(r"comments", CommentViewSet, basename="comments")
 
 urlpatterns = [
     path("feed/", FeedView.as_view(), name="feed"),
+    path("posts/<int:pk>/like/", LikePostView.as_view(), name="like-post"),
+    path("posts/<int:pk>/unlike/", UnlikePostView.as_view(), name="unlike-post"),
 ]
 
 urlpatterns += router.urls
+
+
+
+
